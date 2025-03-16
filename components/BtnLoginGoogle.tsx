314c17beb674de191a71826c8f748f7e14eff987
lookymlive <lookymlive@gmail.com>
2025-03-16 18:31:02 -0300

    Btn google session

--- components/BtnLoginGoogle.tsx
+++ components/BtnLoginGoogle.tsx
@@ -1,10 +1,24 @@
 
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import * as Google from "expo-auth-session/providers/google";
 
 export default function BtnLoginGoogle() {
+ 
+ const [request, response, promptAsync] = Google.useAuthRequest({
+   
+   androidClientId: "",
+   iosClientId: "",
+  
+ })
   return (
-    <Pressable
-     style={styles.btn}>
+    <Pressable style={styles.btn}
+      onPress={() => {
+        promptAsync().catch((e) => {
+          console.error("Error al iniciar sesión con Google:", e);
+        });
+      }}
+    >
+     
       <Text style={{textAlign: "center", color: "white"}}>Login with Google</Text>
     </Pressable>
   );
@@ -22,6 +36,6 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 16,
     fontWeight: "bold",
-  },  
+  },
   }
 );
